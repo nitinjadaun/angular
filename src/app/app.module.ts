547d5a10b188169f41f670c8cdcbaaa441e53191
nitinjaduan <nitin.jaduan@netsolutions.com>
2018-07-04 16:03:22 +0530

    Add componebt

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    DashboardComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
