b8b2805cdd868f71c72de2c772f57cbc88dad95d
nitinjaduan <nitin.jaduan@netsolutions.com>
2018-07-06 19:35:38 +0530

    bootstrap form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+    states = [
+        { id: 1, name: 'Haryana' },
+        { id: 2, name: 'Punjab' },
+        { id: 3, name: 'UP' },
+        { id: 4, name: 'HP' },
+        { id: 5, name: 'MP' },
+        { id: 6, name: 'Gujrat' }
+    ];
+    constructor() { }
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
